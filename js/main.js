ce148cf3f181731e601083e60785d8fb9260eaf1
Salman <bonchy.sm@gmail.com>
2021-04-14 14:08:23 +0300

    files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  new Cocoen(document.querySelector('.cocoen'));
+
+  var owl = $('.owl-carousel')
+
+  owl.owlCarousel({
+    item: 3,
+    loop: true,
+    margin: 18,
+    startPosition: 0,
+    slideTransition: 'ease-in-out',
+    URLhashListener: true,
+    center: true,
+    nav: true,
+    dots: true,
+    responsive:{
+      0:{
+          items: 1.8,
+      },
+      576:{
+          items: 3,
+          margin: 32,
+      }
+    }
+  })
+
+  owl.owlCarousel();
+
+  $('.customPrevBtn').click(function() {
+    owl.trigger('prev.owl.carousel', [300]);
+  })
+
+  $('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+  })
+
+})
